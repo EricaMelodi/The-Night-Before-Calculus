72b0082a174571d6078335c3c8e15640986cdaf2
ericamelodi <gusgeraer@student.gu.se>
2026-02-11 17:39:04 +0100

    Possible to open exit doors

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -2,21 +2,39 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
-    public static bool hasPaper = false;
 
-    private void OnTriggerEnter(Collider other)
+    public Transform player;
+    public float interactDistance = 10f;
+    public float openAngle = 90f;
+    public float openSpeed = 2f;
+
+    private bool isOpen = false;
+    private Quaternion closedRotation;
+    private Quaternion openRotation;
+
+    void Start()
     {
-        if (other.CompareTag("Player"))
+        closedRotation = transform.rotation;
+        openRotation = Quaternion.Euler(transform.eulerAngles + new Vector3(0, openAngle, 0));
+    }
+
+    void Update()
+    {
+        if (Vector3.Distance(player.position, transform.position) <= interactDistance)
         {
-            if (hasPaper)
+            if (Input.GetKeyDown(KeyCode.E))
             {
-                Debug.Log("Door opened!");
-                gameObject.SetActive(false);
+                isOpen = !isOpen;
             }
-            else
-            {
-                Debug.Log("You need the paper first!");
-            }
+        }
+
+        if (isOpen)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, openRotation, Time.deltaTime * openSpeed);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, closedRotation, Time.deltaTime * openSpeed);
         }
     }
 }
