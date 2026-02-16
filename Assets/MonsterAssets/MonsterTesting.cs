29efe1f6b618d33f5fca6b22ad9b84bac4dfc146
01Alekje <01Alexanderkjellberg@gmail.com>
2026-02-16 23:00:39 +0100

    chase music

--- Assets/MonsterAssets/MonsterTesting.cs
+++ Assets/MonsterAssets/MonsterTesting.cs
@@ -22,12 +22,19 @@ public class MonsterAI : MonoBehaviour
     private Vector3 roamTarget;
     private float waitTimer;
 
+    [Header("Sounds")]
+    public AudioSource chaseMusicSource;
+    public AudioClip chaseMusicClip;
+    public float musicVolume;
+    private bool musicPlaying;
+
     private void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         animator = GetComponent<Animator>();
         agent.speed = walkSpeed; // Default to walking
         ChooseRoamTarget();
+        musicPlaying = false;
     }
 
     private void Update()
@@ -40,6 +47,8 @@ public class MonsterAI : MonoBehaviour
             agent.speed = runSpeed; // Set agent speed to running
             agent.SetDestination(player.position);
             animator.SetBool("isRunning", true);
+
+            PlayChaseMusic();
         }
         else
         {
@@ -56,6 +65,8 @@ public class MonsterAI : MonoBehaviour
                     waitTimer = 0f;
                 }
             }
+
+            StopChaseMusic();
         }
 
         // Optional: update animator speed for blend tree
@@ -81,5 +92,27 @@ public class MonsterAI : MonoBehaviour
 
         Gizmos.color = Color.blue;
         Gizmos.DrawWireSphere(transform.position, roamRadius);
+    }
+
+    private void PlayChaseMusic()
+    {
+        if (!musicPlaying)
+        {
+            chaseMusicSource.clip = chaseMusicClip;
+            chaseMusicSource.loop = true;
+            chaseMusicSource.volume = musicVolume;
+            chaseMusicSource.Play();
+            musicPlaying = true;
+        }
+    }
+
+
+    private void StopChaseMusic()
+    {
+        if (musicPlaying)
+        {
+            chaseMusicSource.Stop();
+            musicPlaying = false;
+        }
     }
 }
